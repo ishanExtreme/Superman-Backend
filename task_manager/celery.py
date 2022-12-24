218746e9f2b30729dfb5d5c970ed729b501684e3
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2022-12-24 15:14:21 +0530

    celery status

--- task_manager/celery.py
+++ task_manager/celery.py
@@ -29,24 +29,3 @@ app.conf.beat_schedule = {
     }
 
 }
-
-
-def get_celery_worker_status():
-    i = app.control.inspect()
-    availability = i.ping()
-    stats = i.stats()
-    registered_tasks = i.registered()
-    active_tasks = i.active()
-    scheduled_tasks = i.scheduled()
-    result = {
-        'availability': availability,
-        'stats': stats,
-        'registered_tasks': registered_tasks,
-        'active_tasks': active_tasks,
-        'scheduled_tasks': scheduled_tasks
-    }
-    
-    print(result)
-    
-
-get_celery_worker_status()
