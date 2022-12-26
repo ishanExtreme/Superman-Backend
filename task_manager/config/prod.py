5ea33af098a0dfc4c8322571ad9c5412c9814be7
ishanExtreme <462_bt19@iiitkalyani.ac.in>
2022-12-26 23:48:27 +0530

    sentry added

--- task_manager/config/prod.py
+++ task_manager/config/prod.py
@@ -23,3 +23,22 @@ EMAIL_USE_TLS = True
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_ALLOW_ALL_ORIGINS = True
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=env("SENTRY_URL"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
